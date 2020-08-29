a4823e1d74528812b8242198f86583375290f8d1
chriselswede <chriselswede@gmail.com>
2020-08-29 20:57:47 +0900

    Union and Exception
    Collecting explain plans from a union of the longest average running and total running statements.
    Added an Exception Continuation.

--- hanacollector_before_load_test.sql
+++ hanacollector_before_load_test.sql
@@ -2,5 +2,4 @@
 -- Before the load test with ID LOAD_TEST_2020_11_11_XX reset the SQL plan cache monitoring view and fill info about this test
 alter system reset monitoring view "SYS"."M_SQL_PLAN_STATISTICS_RESET";
 alter system reset monitoring view "SYS"."M_SQL_PLAN_CACHE_RESET";
-insert into "STAT_COLL"."LOAD_TESTS" values ('LOAD_TEST_2020_11_11_XX', CURRENT_TIMESTAMP, Null, 'This is a cool test'); 
---select distinct * from "STAT_COLL"."LOAD_TESTS";
+insert into "STAT_COLL"."LOAD_TESTS" values ('LOAD_TEST_2020_11_11_XX', CURRENT_TIMESTAMP, Null, 'This is a cool test');    --select distinct * from "STAT_COLL"."LOAD_TESTS";
--- hanacollector_init.sql
+++ hanacollector_init.sql
@@ -1,3 +1,4 @@
+--drop schema "STAT_COLL" cascade;
 -- Create a new schema for all the collector tables
 create schema "STAT_COLL";
 
@@ -5,39 +6,33 @@ create schema "STAT_COLL";
 -- These tables must have an additional column with the load test ID
 -- Since we use "select *" we must build something that checks that if new columns been added
 -- to the M_ views, and if so, automatically add the new columns to these collection tables --> TODO
-drop table "STAT_COLL"."STAT_COLL_LOAD_HISTORY_SERVICE"; 
+--drop table "STAT_COLL"."STAT_COLL_LOAD_HISTORY_SERVICE"; 
 create column table "STAT_COLL"."STAT_COLL_LOAD_HISTORY_SERVICE" as (select top 1 * from "SYS"."M_LOAD_HISTORY_SERVICE"); 
 alter table "STAT_COLL"."STAT_COLL_LOAD_HISTORY_SERVICE" add (LOAD_ID varchar(100) default '-1');
-delete from "STAT_COLL"."STAT_COLL_LOAD_HISTORY_SERVICE" where LOAD_ID = '-1';
---select * from "STAT_COLL"."STAT_COLL_LOAD_HISTORY_SERVICE";
+delete from "STAT_COLL"."STAT_COLL_LOAD_HISTORY_SERVICE" where LOAD_ID = '-1';      --select * from "STAT_COLL"."STAT_COLL_LOAD_HISTORY_SERVICE";
 
-drop table "STAT_COLL"."STAT_COLL_LOAD_HISTORY_HOST"; 
+--drop table "STAT_COLL"."STAT_COLL_LOAD_HISTORY_HOST"; 
 create column table "STAT_COLL"."STAT_COLL_LOAD_HISTORY_HOST" as (select top 1 * from "SYS"."M_LOAD_HISTORY_HOST"); 
 alter table "STAT_COLL"."STAT_COLL_LOAD_HISTORY_HOST" add (LOAD_ID varchar(100) default '-1');
-delete from "STAT_COLL"."STAT_COLL_LOAD_HISTORY_HOST" where LOAD_ID = '-1';
---select * from "STAT_COLL"."STAT_COLL_LOAD_HISTORY_HOST";
+delete from "STAT_COLL"."STAT_COLL_LOAD_HISTORY_HOST" where LOAD_ID = '-1';         --select * from "STAT_COLL"."STAT_COLL_LOAD_HISTORY_HOST";
 
-drop table "STAT_COLL"."STAT_COLL_SQL_PLAN_STATISTICS"; 
+--drop table "STAT_COLL"."STAT_COLL_SQL_PLAN_STATISTICS"; 
 create column table "STAT_COLL"."STAT_COLL_SQL_PLAN_STATISTICS" as (select top 1 * from "SYS"."M_SQL_PLAN_STATISTICS_RESET"); 
 alter table "STAT_COLL"."STAT_COLL_SQL_PLAN_STATISTICS" add (LOAD_ID varchar(100) default '-1');
-delete from "STAT_COLL"."STAT_COLL_SQL_PLAN_STATISTICS" where LOAD_ID = '-1';
---select * from "STAT_COLL"."STAT_COLL_SQL_PLAN_STATISTICS"; 
---select * from "SYS"."M_SQL_PLAN_STATISTICS" 
+delete from "STAT_COLL"."STAT_COLL_SQL_PLAN_STATISTICS" where LOAD_ID = '-1';       --select * from "STAT_COLL"."STAT_COLL_SQL_PLAN_STATISTICS"; 
 
-drop table "STAT_COLL"."STAT_COLL_SERVICE_THREAD_SAMPLES"; 
+--drop table "STAT_COLL"."STAT_COLL_SERVICE_THREAD_SAMPLES"; 
 create column table "STAT_COLL"."STAT_COLL_SERVICE_THREAD_SAMPLES" as (select top 1 * from "SYS"."M_SERVICE_THREAD_SAMPLES"); 
 alter table "STAT_COLL"."STAT_COLL_SERVICE_THREAD_SAMPLES" add (LOAD_ID varchar(100) default '-1');
-delete from "STAT_COLL"."STAT_COLL_SERVICE_THREAD_SAMPLES" where LOAD_ID = '-1';
---select * from "STAT_COLL"."STAT_COLL_SERVICE_THREAD_SAMPLES";
+delete from "STAT_COLL"."STAT_COLL_SERVICE_THREAD_SAMPLES" where LOAD_ID = '-1';    --select * from "STAT_COLL"."STAT_COLL_SERVICE_THREAD_SAMPLES";
 
-drop table "STAT_COLL"."STAT_COLL_EXPLAIN_PLANS"; 
+--drop table "STAT_COLL"."STAT_COLL_EXPLAIN_PLANS"; 
 create column table "STAT_COLL"."STAT_COLL_EXPLAIN_PLANS" as (select top 1 * from "SYS"."EXPLAIN_PLAN_TABLE"); 
 alter table "STAT_COLL"."STAT_COLL_EXPLAIN_PLANS" add (LOAD_ID varchar(100) default '-1');
 alter table "STAT_COLL"."STAT_COLL_EXPLAIN_PLANS" add (STATEMENT_HASH varchar(32) default '-1');
-delete from "STAT_COLL"."STAT_COLL_EXPLAIN_PLANS" where LOAD_ID = '-1';
---select * from "STAT_COLL"."STAT_COLL_EXPLAIN_PLANS";
+delete from "STAT_COLL"."STAT_COLL_EXPLAIN_PLANS" where LOAD_ID = '-1';              --select * from "STAT_COLL"."STAT_COLL_EXPLAIN_PLANS";
 
-drop table "STAT_COLL"."LOAD_TESTS";                               - time zone server  or  UTC ?
+--drop table "STAT_COLL"."LOAD_TESTS";                               - time zone server  or  UTC ?
 create column table "STAT_COLL"."LOAD_TESTS"(TEST_ID VARCHAR(100), START_TIME DATETIME, STOP_TIME DATETIME, TEST_COMMENT VARCHAR(500));
 
 
@@ -61,56 +56,55 @@ LANGUAGE SQLSCRIPT
 SQL SECURITY DEFINER
 AS
 BEGIN
-       -- Collect the Execution Plans of the 5 most "expensive" (here: longest average execution time) from the Reset SQL Plan Cache
-       -- and they must be of such SQL statements that support explain plan (note, here we cannot user M_SLQ_PLAN_STATISTICS, some plan ids are invalid)
-       DECLARE CURSOR cur FOR select top 5 PLAN_ID, STATEMENT_STRING, STATEMENT_HASH from "SYS"."M_SQL_PLAN_CACHE_RESET" 
+    -- Collect the Execution Plans of the 100 (currently 17) most "expensive" w.r.t. average execution time and 100 (currently 17) most "expensive" w.r.t. total execution time from the Reset SQL Plan Cache
+	-- Note1: they must be of such SQL statements that support explain plan
+	-- Note2: here we cannot user M_SLQ_PLAN_STATISTICS, some plan ids are invalid
+	DECLARE all_statements TABLE(PLAN_ID BIGINT, STATEMENT_STRING NCLOB, STATEMENT_HASH VARCHAR(32), AVG_EXECUTION_TIME BIGINT, TOTAL_EXECUTION_TIME BIGINT) = select PLAN_ID, STATEMENT_STRING, STATEMENT_HASH, AVG_EXECUTION_TIME, TOTAL_EXECUTION_TIME from "SYS"."M_SQL_PLAN_STATISTICS_RESET" 
              where STATEMENT_STRING like 'INSERT%' 
              or STATEMENT_STRING like 'UPDATE%' 
              or STATEMENT_STRING like 'DELETE%' 
              or STATEMENT_STRING like 'REPLACE%' 
              or STATEMENT_STRING like 'UPSERT%' 
              or STATEMENT_STRING like 'MERGE INTO%' 
-             or STATEMENT_STRING like 'SELECT%'
-             order by AVG_EXECUTION_TIME desc; 
-       DECLARE plan_id_str VARCHAR(100);
-       DECLARE sql_str VARCHAR(10000);
-       DECLARE sql_hash VARCHAR(32);
-       FOR cur_row AS cur DO
-       sql_str := cur_row.STATEMENT_STRING;               
-       --select :sql_str from dummy;
-       plan_id_str := CAST(:cur_row.PLAN_ID AS VARCHAR);
-       --select :plan_id_str from dummy;
-       sql_hash := cur_row.STATEMENT_HASH;
-       --select :sql_hash from dummy;
+             or STATEMENT_STRING like 'SELECT%';
+	DECLARE avgdesc  TABLE(PLAN_ID BIGINT, STATEMENT_HASH VARCHAR(32)) = select top 17 PLAN_ID, STATEMENT_HASH from :all_statements order by avg_execution_time desc;
+	DECLARE totdesc  TABLE(PLAN_ID BIGINT, STATEMENT_HASH VARCHAR(32)) = select top 17 PLAN_ID, STATEMENT_HASH from :all_statements order by total_execution_time desc;
+	DECLARE unionsel TABLE(PLAN_ID BIGINT, STATEMENT_HASH VARCHAR(32)) = select * from :avgdesc union select * from :totdesc;        
+	DECLARE CURSOR cur FOR select * from :unionsel;
+    DECLARE plan_id_str VARCHAR(100);
+    DECLARE sql_hash VARCHAR(32);
+    --DECLARE CONTINUE HANDLER FOR SQL_ERROR_CODE 428 SELECT ::SQL_ERROR_CODE, ::SQL_ERROR_MESSAGE FROM DUMMY;        -- to catch when EXPLAIN PLAN SET gets an "invalid" plan-id
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SELECT ::SQL_ERROR_CODE, ::SQL_ERROR_MESSAGE FROM DUMMY;
+    FOR cur_row AS cur DO
+       plan_id_str := CAST(:cur_row.PLAN_ID AS VARCHAR);     
+       sql_hash := cur_row.STATEMENT_HASH;                     -- select :plan_id_str, :sql_hash from dummy;
        delete from sys.explain_plan_table where statement_name = :plan_id_str;
-       EXEC 'EXPLAIN PLAN SET STATEMENT_NAME = '''||plan_id_str||''' FOR SQL PLAN CACHE ENTRY '||plan_id_str||' ';
-       --SELECT * FROM sys.explain_plan_table WHERE statement_name = :plan_id_str;
-       INSERT INTO "STAT_COLL"."STAT_COLL_EXPLAIN_PLANS" SELECT *, :ID as "LOAD_ID", :sql_hash as "STATEMENT_HASH" 
-             FROM sys.explain_plan_table WHERE statement_name = :plan_id_str;
+       EXEC 'EXPLAIN PLAN SET STATEMENT_NAME = '''||plan_id_str||''' FOR SQL PLAN CACHE ENTRY '||plan_id_str||' ';  -- it happens that the plan-id is "invalid", then the CONTINUE HANDLER
+       INSERT INTO "STAT_COLL"."STAT_COLL_EXPLAIN_PLANS" SELECT *, :ID as "LOAD_ID", :sql_hash as "STATEMENT_HASH"  -- will give a warning and the EXPLAIN PLAN SET will not be done, and  
+             FROM sys.explain_plan_table WHERE statement_name = :plan_id_str;                                       -- this INSERT INTO will not insert anything since that plan id is not there
     END FOR;
     -- Collect all statistics from the SQL Plan Cache since the reset 
-       INSERT INTO "STAT_COLL"."STAT_COLL_SQL_PLAN_STATISTICS" SELECT TOP 1000 *, :ID AS "LOAD_ID"
+    INSERT INTO "STAT_COLL"."STAT_COLL_SQL_PLAN_STATISTICS" SELECT TOP 1000 *, :ID AS "LOAD_ID"
        FROM "SYS"."M_SQL_PLAN_STATISTICS_RESET"  
        WHERE SCHEMA_NAME = 'SAPQH1' AND IS_INTERNAL = 'FALSE'  -- Change SAPQH1 --> SAP<SID> or similar and add   AND RESET_TIME IS NOT NULL  TODO:input parameter
        ORDER BY TOTAL_EXECUTION_TIME;
     -- If start time is not defined as an input parameter, get the start time from the LOAD_TESTS table for this test
-       IF (:START_TIME_IN is NULL) THEN
-             select distinct START_TIME into START_TIME_IN from "STAT_COLL"."LOAD_TESTS" where TEST_ID = :ID;
-       END IF;
-       -- If stop time is not defined as an input parameter, set current time as the stop time (since this procedure will be called right after the test)
-       IF (:STOP_TIME_IN is NULL) THEN
-             select CURRENT_TIMESTAMP into STOP_TIME_IN from dummy;
-       END IF;
-       -- Update the LOAD_TESTS table with start and stop time
-       update "STAT_COLL"."LOAD_TESTS" SET START_TIME = :START_TIME_IN where TEST_ID = :ID;
-       update "STAT_COLL"."LOAD_TESTS" SET STOP_TIME = :STOP_TIME_IN where TEST_ID = :ID; 
-       --select * from "STAT_COLL"."LOAD_TESTS";
-       -- Collect all statistics from M_LOAD_HISTORY_SERVICE between the start and stop time
-       INSERT INTO "STAT_COLL"."STAT_COLL_LOAD_HISTORY_SERVICE" SELECT *, :ID AS "LOAD_ID"
+    IF (:START_TIME_IN is NULL) THEN
+       select distinct START_TIME into START_TIME_IN from "STAT_COLL"."LOAD_TESTS" where TEST_ID = :ID;
+    END IF;
+    -- If stop time is not defined as an input parameter, set current time as the stop time (since this procedure will be called right after the test)
+    IF (:STOP_TIME_IN is NULL) THEN
+       select CURRENT_TIMESTAMP into STOP_TIME_IN from dummy;
+    END IF;
+    -- Update the LOAD_TESTS table with start and stop time
+    update "STAT_COLL"."LOAD_TESTS" SET START_TIME = :START_TIME_IN where TEST_ID = :ID;
+    update "STAT_COLL"."LOAD_TESTS" SET STOP_TIME = :STOP_TIME_IN where TEST_ID = :ID;        --select * from "STAT_COLL"."LOAD_TESTS";
+    -- Collect all statistics from M_LOAD_HISTORY_SERVICE between the start and stop time
+    INSERT INTO "STAT_COLL"."STAT_COLL_LOAD_HISTORY_SERVICE" SELECT *, :ID AS "LOAD_ID"
        FROM "SYS"."M_LOAD_HISTORY_SERVICE" 
        WHERE TIME BETWEEN START_TIME_IN AND STOP_TIME_IN ORDER BY TIME;
     -- Collect all statistics from M_LOAD_HISTORY_HOST between the start and stop time
-       INSERT INTO "STAT_COLL"."STAT_COLL_LOAD_HISTORY_HOST" SELECT *, :ID AS "LOAD_ID"
+    INSERT INTO "STAT_COLL"."STAT_COLL_LOAD_HISTORY_HOST" SELECT *, :ID AS "LOAD_ID"
        FROM "SYS"."M_LOAD_HISTORY_HOST" 
        WHERE TIME BETWEEN START_TIME_IN AND STOP_TIME_IN ORDER BY TIME;
     -- Collect all statistics from M_SERVICE_THREAD_SAMPLES between the start and stop time
